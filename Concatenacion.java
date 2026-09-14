1f8ad35599fff749f2755e884fb47973b790851d
arirllegp <garciapaucarkevin@gmail.com>
2026-09-14 09:13:59 -0500

    Add files via upload

---
+++ Concatenacion.java
@@ -0,0 +1,10 @@
+class Concatenacion {
+    public static void main(String[] args) {
+        String nombre = "John";
+        String primerApellido = "Doe";
+
+        String nombreCompleto = nombre + " " + primerApellido;
+
+        System.out.println("Nombre completo: " + nombreCompleto);
+    } // Fin del método main
+} // Fin de la clase Concatenacion
